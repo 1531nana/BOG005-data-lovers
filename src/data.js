89a7e838faacab3c44a666f4ad6884788168951c
jeniferSamper <jenifersamperusb@gmail.com>
2022-08-04 15:16:36 -0500

    prueba del .sort

--- src/data.js
+++ src/data.js
@@ -1,24 +1,62 @@
 
-export const showImage = (element) => {
-  //  let imageCharacter = Object.values(element);
-  //  let listImage = imageCharacter[1].map(total => total.image);
-   let listImage = element['results'].map(total => total.image);
-  //  console.log(listImage);
-     return listImage;
-};
+// export const showImage = (element) => {
+//    let listImage = element['results'].map(total => total.image);
+//      return listImage;
+// };
 
 
-export const showName = (element) => {
-  // let nameCharacter = Object.values(element);
-  // // console.log(nameCharacter);
-  // let listName = nameCharacter[1].map(total => total.name);
-  let listName = element['results'].map(total => total.name);
+// export const showName = (element) => {
+//   let listName = element['results'].map(total => total.name);
+//   return listName;
+// };
 
-  return listName;
-};
+// export const showOrder = (element) => {
+//   let dataToOrder = new Object();
+//   dataToOrder.image = element['results'].map(total => total.image);
+//   dataToOrder.name=element['results'].map(total => total.name);
+//   console.log('objeto datatoorder', dataToOrder);
+//   return dataToOrder
+// }
+export const showData = (element) => {
+let listImage = element['results'].map(total => total.image);
+let listName = element['results'].map(total => total.name);
+//estructura del objeto 
+function createItem  (image, name){
+  this.image = image;
+  this.name = name;
+}
+
+let createObject; 
+let dataToOrder= [];
+for(let i = 0 ; i<listName.length; i++){
+  createObject = new createItem (listImage[i], listName[i]);
+  dataToOrder.push(createObject);
+}
+// console.log('ver crea objeto', createObject )
+// console.log('ver dataorder', dataToOrder)
+
+return dataToOrder
+}
+
+export const showOrder = (element) => {
+
+  function compareName(a,b){
+  
+  if ( a.name.toLowerCase() < b.name.toLowerCase()){
+    return -1;
+  }
+  if ( a.name.toLowerCase() > b.name.toLowerCase()){
+    return 1;
+  }
+  return 0;
+}
+element.sort(compareName);
+console.log("ordenado", element);
+return element;
+}
 
 
-// export const showOrder = (a, b) => {
+// export const showData = (a, b) => {
 // // let sortImage = showImage();
 // // let sortName = showName();
 // // let sortImageAndName = sortImage + sortName;
--- src/main.js
+++ src/main.js
@@ -1,90 +1,184 @@
-import {showImage, showName} from './data.js';
+//import {showImage, showName, showOrder} from './data.js';
+import {showData, showOrder} from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 let dataRyM = data;
-let showImages = showImage(dataRyM);
-let showNames = showName(dataRyM);
-
+// let showImages = showImage(dataRyM);
+// let showNames = showName(dataRyM);
+let showDatas = showData(dataRyM);
+// console.log('mirar', showDatas);
+// let showOrderAZ = showOrder(showDatas);
+// console.log("se ordeno o no",showOrderAZ)
 
 let acum = "";
 let addDiv;
 let screen1 = document.getElementById("screen1");
-let sizeData = showImages.length;
-console.log(sizeData);
-let divideData = Math.round(sizeData / 33);
-console.log(divideData);
+//let sizeData = showImages.length;
+let sizeData = showDatas.length
+
 let pageStart = document.getElementById("page_start");
 let pageCharacteres = document.getElementById("page_characteres");
 let btnCharacteres = document.getElementById("button_characteres");
 let btnRigth = document.getElementById("rigth");
 let btnLeft = document.getElementById("left");
+let navInit = document.getElementById("init");
+let navAZ = document.getElementById("order_alphabetic");
+
+
+
 let positionStart = -15;
 let positionEnd = 0;
 pageStart.style.display = "visible";
 pageCharacteres.style.display = "none";
 btnLeft.style.display = "none";
 
-    // funcion para la flecha derecha
+   
 
     const rigth = () => {
-        positionStart +=15;
-        positionEnd += 15;
-        pageStart.style.display = "none";
-        pageCharacteres.style.display = "block";
-        screen1.style.display = "visible";
-        screen1.innerHTML = "";
-        if(positionStart >= 15){
-            btnLeft.style.display = "block";
+            positionStart +=15;
+            positionEnd += 15;
+            pageStart.style.display = "none";
+            pageCharacteres.style.display = "block";
+            screen1.style.display = "visible";
+            screen1.innerHTML = "";
+            if(positionStart >= 15){
+                btnLeft.style.display = "block";
+            }
+            acum = "";
+            for (let i = positionStart; i < positionEnd; i++) {
+                if (i >= sizeData) {
+                    btnRigth.style.display = "none";
+                    return;
+                } else {
+                    addDiv = `
+                    <div id=${"div" + i} className=${"character"}>
+                    <img src=${showDatas[i].image} id=${"img" + i}>
+                    <span id=${"name" + i}>${showDatas[i].name}</span>
+                    </div>
+                    `
+                    acum += addDiv;
+                    screen1.innerHTML = acum;
+                }
+        
+            }
         }
-        acum = "";
-        console.log(positionStart, positionEnd);
-        for (let i = positionStart; i < positionEnd; i++) {
-            if (i >= sizeData) {
-                btnRigth.style.display = "none";
-                return;
-            } else {
+        btnCharacteres.addEventListener("click", rigth);
+        btnRigth.addEventListener("click", rigth);
+        
+        // funcion para la flecha izquierda
+        const left= () => {
+            positionStart -= 15;
+            positionEnd -= 15;
+            pageStart.style.display = "none";
+            pageCharacteres.style.display = "block";
+            screen1.style.display = "visible";
+            btnRigth.style.display ="block";
+            if(positionStart < 0 ){
+            positionStart = 0;
+            positionEnd = 15;
+            btnLeft.style.display = "none";
+            return;
+            } else if(positionStart >= 15){
+                btnLeft.style.display = "block";
+            }
+            screen1.innerHTML = "";
+            acum = "";
+            for(let i = positionStart; i < positionEnd; i++ ){
                 addDiv = `
                 <div id=${"div" + i} className=${"character"}>
-                <img src=${showImages[i]} id=${"img" + i}>
-                <span id=${"name" + i}>${showNames[i]}</span>
+                <img src=${showDatas[i].image} id=${"img" + i}>
+                    <span id=${"name" + i}>${showDatas[i].name}</span>
                 </div>
                 `
-                acum += addDiv;
+                acum+=addDiv;
                 screen1.innerHTML = acum;
             }
+        }
+        btnLeft.addEventListener("click", left);
+
+        const home = () =>{
+            pageStart.style.display = "block";
+            pageCharacteres.style.display = "none";
+            positionStart=-15;
+            positionEnd= 0;
+        }
+        navInit.addEventListener("click",home)
+// if(navAZ.addEventListener("click", showOrder(showDatas))){
+//     console.log("prueba")
+// }
+        const ordenalo =() => {
+            let prueba= showOrder(showDatas);
+            console.log("resultado de ordenalo",prueba);
+            positionStart=-15;
+            positionEnd= 0;
+            rigth(); 
+        }
+        navAZ.addEventListener("click", ordenalo)
+        
     
-        }
-    }
-    btnCharacteres.addEventListener("click", rigth);
-    btnRigth.addEventListener("click", rigth);
+      
+        // codigo viejo
+         // funcion para la flecha derecha
+
+    // const rigth = () => {
+    //     positionStart +=15;
+    //     positionEnd += 15;
+    //     pageStart.style.display = "none";
+    //     pageCharacteres.style.display = "block";
+    //     screen1.style.display = "visible";
+    //     screen1.innerHTML = "";
+    //     if(positionStart >= 15){
+    //         btnLeft.style.display = "block";
+    //     }
+    //     acum = "";
+    //     console.log(positionStart, positionEnd);
+    //     for (let i = positionStart; i < positionEnd; i++) {
+    //         if (i >= sizeData) {
+    //             btnRigth.style.display = "none";
+    //             return;
+    //         } else {
+    //             addDiv = `
+    //             <div id=${"div" + i} className=${"character"}>
+    //             <img src=${showImages[i]} id=${"img" + i}>
+    //             <span id=${"name" + i}>${showNames[i]}</span>
+    //             </div>
+    //             `
+    //             acum += addDiv;
+    //             screen1.innerHTML = acum;
+    //         }
     
-    // funcion para la flecha izquierda
-    const left= () => {
-        console.log("posición inicial"+positionStart, "posición final"+positionEnd)
-        positionStart -= 15;
-        positionEnd -= 15;
-        pageStart.style.display = "none";
-        pageCharacteres.style.display = "block";
-        screen1.style.display = "visible";
-        btnRigth.style.display ="block";
-        if(positionStart < 0 ){
-        positionStart = 0;
-        positionEnd = 15;
-        btnLeft.style.display = "none";
-        return;
-        } else if(positionStart >= 15){
-            btnLeft.style.display = "block";
-        }
-        screen1.innerHTML = "";
-        acum = "";
-        for(let i = positionStart; i < positionEnd; i++ ){
-            addDiv = `
-            <div id=${"div" + i} className=${"character"}>
-            <img src=${showImages[i]} id=${"img" + i}>
-            <span id=${"name" + i}>${showNames[i]}</span>
-            </div>
-            `
-            acum+=addDiv;
-            screen1.innerHTML = acum;
-        }
-    }
-    btnLeft.addEventListener("click", left);
+    //     }
+    // }
+    // btnCharacteres.addEventListener("click", rigth);
+    // btnRigth.addEventListener("click", rigth);
+    
+    // // funcion para la flecha izquierda
+    // const left= () => {
+    //     console.log("posición inicial"+positionStart, "posición final"+positionEnd)
+    //     positionStart -= 15;
+    //     positionEnd -= 15;
+    //     pageStart.style.display = "none";
+    //     pageCharacteres.style.display = "block";
+    //     screen1.style.display = "visible";
+    //     btnRigth.style.display ="block";
+    //     if(positionStart < 0 ){
+    //     positionStart = 0;
+    //     positionEnd = 15;
+    //     btnLeft.style.display = "none";
+    //     return;
+    //     } else if(positionStart >= 15){
+    //         btnLeft.style.display = "block";
+    //     }
+    //     screen1.innerHTML = "";
+    //     acum = "";
+    //     for(let i = positionStart; i < positionEnd; i++ ){
+    //         addDiv = `
+    //         <div id=${"div" + i} className=${"character"}>
+    //         <img src=${showImages[i]} id=${"img" + i}>
+    //         <span id=${"name" + i}>${showNames[i]}</span>
+    //         </div>
+    //         `
+    //         acum+=addDiv;
+    //         screen1.innerHTML = acum;
+    //     }
+    // }
+    // btnLeft.addEventListener("click", left);
