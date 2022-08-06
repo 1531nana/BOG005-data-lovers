5c7979a73adf7e8b6315aadc9987a6bcf2029c7b
jeniferSamper <jenifersamperusb@gmail.com>
2022-08-06 13:48:01 -0500

    datos aleatorios

--- src/data.js
+++ src/data.js
@@ -1,104 +1,142 @@
 
-// export const showImage = (element) => {
-//    let listImage = element['results'].map(total => total.image);
-//   //  let imageCharacter = Object.values(element);
-//   //  let listImage = imageCharacter[1].map(total => total.image);
-//    let listImage = element['results'].map(total => total.image);
-//   //  console.log(listImage);
-//      return listImage;
-// };
-
-
-// export const showName = (element) => {
-//   // let nameCharacter = Object.values(element);
-//   // // console.log(nameCharacter);
-//   // let listName = nameCharacter[1].map(total => total.name);
-//   let listName = element['results'].map(total => total.name);
-
-//   return listName;
-// };
-
 export const showData = (element) => {
   let listImage = element['results'].map(total => total.image);
   let listName = element['results'].map(total => total.name);
   let listSpecies = element['results'].map(total => total.species);
+  let listGender = element['results'].map(total => total.gender);
+  let listStatus = element['results'].map(total => total.status);
 
-  function createItem(name, image, species) {
+  function createItem(name, image, species, gender, status) {
     this.name = name;
     this.image = image;
     this.species = species;
+    this.gender = gender;
+    this.status = status;
   }
   let dataToOrder = [];
 
-  for(let i = 0; i < listImage.length; i++){
-    let createObject = new createItem(listName[i], listImage[i], listSpecies[i]);  
+  for (let i = 0; i < listImage.length; i++) {
+    let createObject = new createItem(listName[i], listImage[i], listSpecies[i], listGender[i], listStatus[i]);
     dataToOrder.push(createObject);
   }
-  // console.log("for "+dataToOrder);
-return dataToOrder
+  return dataToOrder
 }
 
 
 export const showOrder = (element) => {
 
-  const compareName =(a, b) => {
+  const compareName = (a, b) => {
 
-    if ( a.name.toLowerCase() < b.name.toLowerCase()){
+    if (a.name.toLowerCase() < b.name.toLowerCase()) {
       return -1;
     }
-    if ( a.name.toLowerCase() > b.name.toLowerCase()){
+    if (a.name.toLowerCase() > b.name.toLowerCase()) {
       return 1;
     }
     return 0;
   }
 
-   element.sort(compareName);
-  // console.log(information);
+  element.sort(compareName);
+
   return element;
 
 }
 
 export const filterHuman = (element) => {
-  let filtro = element.filter(element =>  element.species == "Human") 
-  return filtro}
+  let filtro = element.filter(element => element.species == "Human")
+  return filtro
+}
 
-  export const filterAlien = (element) => {
-    let filtros = element.filter(element =>  element.species == "Alien") 
-    console.log(filtros);
-    return filtros}
-  
-//     if(element.species){
-//      return humans.push(element);
+export const filterAlien = (element) => {
+  let filtros = element.filter(element => element.species == "Alien")
+  return filtros;
+}
+
+//   export const dataCurious = (element)=> {
+//     let genderF = 0;
+//     let genderM = 0;
+//     let genderU = 0;
+//         element.forEach(element => {
+//       if(element.gender== "Female"){
+//        genderF ++;
+//       } else if (element.gender == "Male"){
+//         genderM ++;
+//       } else {
+//         genderU ++;
+//   }});
+//   let dataGender = [];
+// dataGender.push(genderF, genderM, genderU);
+
+//   let statusA = 0;
+//   let statusD = 0;
+//   let statusU = 0;
+//   element.forEach(element => {
+//     if(element.status == "Alive"){
+//       statusA ++;
+//     } else if (element.status == "Dead"){
+//       statusD ++;
+//     } else {
+//       statusU ++; 
+//     }});
+//     let dataStatus = [];
+//     dataStatus.push(statusA, statusD, statusU);
+
+//     let dataRandom = [];
+//     dataRandom.push(dataGender);
+//     dataRandom.push(dataStatus);
+//   return (dataRandom)
 //   }
-//   if(element.species == "Aliens"){
-//     return aliens.push(element);
-//   }
-//   // console.log(filtro);
-// }
 
-// )
-// return filtro, aliens, humans;
-// }
+export const dataCurious = (element) => {
+  let dataRandom = [];
 
-// export const ordenar = (element) =>{
-//   let order = element.map(showName().concat(showImage()));
-//   return order;
-// }
+  let genderF = 0;
+  let genderM = 0;
+  let genderU = 0;
+  element.forEach(element => {
+    if (element.gender == "Female") {
+      genderF++;
+    } else if (element.gender == "Male") {
+      genderM++;
+    } else {
+      genderU++;
+    }
+  });
 
-// export const showData = (a, b) => {
-// // let sortImage = showImage();
-// // let sortName = showName();
-// // let sortImageAndName = sortImage + sortName;
-// if(a.name < b.name ){
-// return -1;
-// } else if (a.name > b.name){
-//   return 1;
-// }
-// return 0;
-// }
+  let statusA = 0;
+  let statusD = 0;
+  let statusU = 0;
+  element.forEach(element => {
+    if (element.status == "Alive") {
+      statusA++;
+    } else if (element.status == "Dead") {
+      statusD++;
+    } else {
+      statusU++;
+    }
+  });
 
-// export const showNameAndImageSort = (element) => {
-//   let nameAndImageSort = showName().sort(element)
-//   return nameAndImageSort;
-//   // element['results'].sort((total => total.name))
-// }
+  // objeto pero es mas facil crear el objeto directo
+  // function createDataCurious (female, male, unknown){
+  //   this.female = female;
+  //   this.male = male;
+  //   this.unknown = unknown;}
+let dataGender = {
+  Female: genderF,
+  Male: genderM,
+  Unknown: genderU,
+};
+dataRandom.push(dataGender);
+
+let dataStatus = {
+  Alive: statusA,
+  Dead: statusD,
+  Unknown: statusU,
+}
+dataRandom.push(dataStatus);
+
+
+// let createObjectGender = new createDataCurious (genderF, genderM, genderU);
+console.log("datarandom", dataRandom)
+return dataRandom;
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { showData, showOrder, filterHuman, filterAlien} from './data.js';
+import { showData, showOrder, filterHuman, filterAlien, dataCurious } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 let dataRyM = data;
 let showDatas = showData(dataRyM);
+// console.log("imprimir data", showDatas);
+
 let btnInit = document.getElementById("init");
 let acum = "";
 let addDiv;
 let screen1 = document.getElementById("screen1");
 let sizeData = showDatas.length;
-console.log(sizeData);
-// let divideData = Math.round(sizeData / 33);
-// console.log(divideData);
+
+
 // variables del DOM
 let btnHuman = document.getElementById("human");
 let btnAlien = document.getElementById("alien");
@@ -27,124 +28,150 @@ pageStart.style.display = "visible";
 pageCharacteres.style.display = "none";
 btnLeft.style.display = "none";
 
-    // funcion para la flecha derecha
+// funcion para la flecha derecha
 
-    const rigth = () => {
-        positionStart +=15;
-        positionEnd += 15;
-        pageStart.style.display = "none";
-        pageCharacteres.style.display = "block";
-        screen1.style.display = "visible";
-        screen1.innerHTML = "";
-        if(positionStart >= 15){
-            btnLeft.style.display = "block";
-        }
-        acum = "";
-        console.log(positionStart, positionEnd);
-        for (let i = positionStart; i < positionEnd; i++) {
-            if (i >= sizeData) {
-                btnRigth.style.display = "none";
-                return;
-            }  else {
-                    addDiv = `
+const rigth = () => {
+    positionStart += 15;
+    positionEnd += 15;
+    pageStart.style.display = "none";
+    pageCharacteres.style.display = "block";
+    screen1.style.display = "visible";
+    screen1.innerHTML = "";
+    if (positionStart >= 15) {
+        btnLeft.style.display = "block";
+    }
+    acum = "";
+    
+    for (let i = positionStart; i < positionEnd; i++) {
+        if (i >= sizeData) {
+            btnRigth.style.display = "none";
+            return;
+        } else {
+            addDiv = `
                     <div id=${"div" + i} className=${"character"}>
                     <img src=${showDatas[i].image} id=${"img" + i}>
                     <span id=${"name" + i}>${showDatas[i].name}</span>
                     </div>
                     `
-                acum += addDiv;
-                screen1.innerHTML = acum;
-            }
-    
+            acum += addDiv;
+            screen1.innerHTML = acum;
         }
+
     }
-    btnCharacteres.addEventListener("click", rigth);
-    btnRigth.addEventListener("click", rigth);
-    
-    // funcion para la flecha izquierda
-    const left= () => {
-        console.log("posición inicial"+positionStart, "posición final"+positionEnd)
-        positionStart -= 15;
-        positionEnd -= 15;
-        pageStart.style.display = "none";
-        pageCharacteres.style.display = "block";
-        screen1.style.display = "visible";
-        btnRigth.style.display ="block";
-        if(positionStart < 0 ){
+}
+btnCharacteres.addEventListener("click", rigth);
+btnRigth.addEventListener("click", rigth);
+
+// funcion para la flecha izquierda
+const left = () => {
+    positionStart -= 15;
+    positionEnd -= 15;
+    pageStart.style.display = "none";
+    pageCharacteres.style.display = "block";
+    screen1.style.display = "visible";
+    btnRigth.style.display = "block";
+    if (positionStart < 0) {
         positionStart = 0;
         positionEnd = 15;
         btnLeft.style.display = "none";
         return;
-        } else if(positionStart >= 15){
-            btnLeft.style.display = "block";
-        }
-        screen1.innerHTML = "";
-        acum = "";
-        for(let i = positionStart; i < positionEnd; i++ ){
-            addDiv = `
+    } else if (positionStart >= 15) {
+        btnLeft.style.display = "block";
+    }
+    screen1.innerHTML = "";
+    acum = "";
+    for (let i = positionStart; i < positionEnd; i++) {
+        addDiv = `
             <div id=${"div" + i} className=${"character"}>
             <img src=${showDatas[i].image} id=${"img" + i}>
              <span id=${"name" + i}>${showDatas[i].name}</span>
              </div>
             `
-            acum+=addDiv;
-            screen1.innerHTML = acum;
-        }
+        acum += addDiv;
+        screen1.innerHTML = acum;
     }
-    btnLeft.addEventListener("click", left);
+}
+btnLeft.addEventListener("click", left);
 
 //función para volver a la página de inicio
-        const home = () =>{
-            pageCharacteres.style.display = "none";
-            pageStart.style.display = "grid";
-            positionStart = -15;
-            positionEnd = 0;
-            showDatas = showData(dataRyM);
-        }
+const home = () => {
+    pageCharacteres.style.display = "none";
+    pageStart.style.display = "grid";
+    positionStart = -15;
+    positionEnd = 0;
+    showDatas = showData(dataRyM);
+}
 btnInit.addEventListener("click", home);
 
 // función para el botón ordenar de la A-Z
-const selectOrder = () =>{
- showDatas = showData(dataRyM);
- let orderImageAndName = showOrder(showDatas);
- console.log(orderImageAndName);
- positionStart = -15;
- positionEnd = 0;
- rigth();
+const selectOrder = () => {
+    showDatas = showData(dataRyM);
+    showDatas = showOrder(showDatas);
+    positionStart = -15;
+    positionEnd = 0;
+    rigth();
 
 }
 btnOrderAZ.addEventListener("click", selectOrder);
 
 // función para el botón ordenar de la A-Z
 const orderReverse = () => {
- showDatas = showData(dataRyM);
- let orderImageAndName = showOrder(showDatas);
- orderImageAndName.reverse();
- positionStart = -15;
- positionEnd = 0;
- rigth();
+    showDatas = showData(dataRyM);
+    showDatas = showOrder(showDatas);
+    showDatas.reverse();
+    positionStart = -15;
+    positionEnd = 0;
+    rigth();
 }
 btnOrderZA.addEventListener("click", orderReverse);
 
 //función para filtrar por especies humanas
 const filtersHuman = () => {
     showDatas = showData(dataRyM);
-     showDatas = filterHuman(showDatas);
-     sizeData = showDatas.length;
-        positionStart = -15;
-        positionEnd = 0;
-        rigth();
-    }    
+    showDatas = filterHuman(showDatas);
+    sizeData = showDatas.length;
+    positionStart = -15;
+    positionEnd = 0;
+    rigth();
+}
 btnHuman.addEventListener("click", filtersHuman)
 
 //función para filtrar por especies aliens
 const filtersAliens = () => {
     showDatas = showData(dataRyM);
     showDatas = filterAlien(showDatas);
-    console.log(showDatas);
     sizeData = showDatas.length;
-       positionStart = -15;
-       positionEnd = 0;
-       rigth();
-   }  
+    positionStart = -15;
+    positionEnd = 0;
+    rigth();
+}
 btnAlien.addEventListener("click", filtersAliens);
+
+//funcion para imprimir datos 
+const showDatasCurious =() =>{
+    showDatas =showData(dataRyM);
+    btnRigth.style.display = "none";
+    btnLeft.style.display = "none";
+    screen1.innerHTML = "";
+    acum = "";
+    // document.getElementById("portal").style.display="block"
+    let dataRandom = dataCurious(showDatas);
+    //elige aleatorio el dato
+    let dataPrint = dataRandom[Math.floor(Math.random()*dataRandom.length)];
+    console.log("datos 0 o 1", dataPrint)
+    //elige aleatorio la llave
+    let keyObject =Object.keys(dataPrint);
+    keyObject = keyObject[Math.floor(Math.random()*keyObject.length)];
+    console.log("llave", keyObject)
+    //imprime el valor de la llave
+    let keyObjectValue = dataPrint[`${keyObject}`]
+    console.log("valor llave", keyObjectValue)
+    //imprimir en pantalla
+    acum= ` <div id=portaldatos>
+    <label id="printCuriousFact">Hay ${keyObjectValue} personajes ${keyObject} en Rick and Morty </label>
+    </div>`
+    screen1.innerHTML = acum;
+}
+document.getElementById("curious_fact").addEventListener("click", showDatasCurious)
+
+// console.log("data gender, data status", dataCurious(showDatas))
